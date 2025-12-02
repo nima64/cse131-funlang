74da6da649dae1404dfac60248afd0e67ee9a409
nima64 <kennethe407@gmail.com>
2025-12-01 18:47:48 -0800

    new tests for type checking functions and loops. Old repl tests also updated to match new format

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -9,6 +9,9 @@ success_tests! {
     type_test_6_loop_num: { file: "type_test_6_loop_num", input: "50", expected: "100" },
     type_test_7_if_any: { file: "type_test_7_if_any", input: "42", expected: "42" },
     type_test_9_input_cast: { file: "type_test_9_input_cast", input: "5", expected: "8" },
+    type_test_fib: { file: "type_test_fib", input: "", expected: "1" },
+    type_test_frog_nested_loops: { file: "type_test_frog_nested_loops", input: "4", expected: "3\n2\n1\ntrue" , typecheck: true},
+    type_test_mutual_recursion: { file: "type_test_mutual_recursion", input: "4", expected: "4\ntrue\ntrue" , typecheck: true},
 
     test_input: { file: "input", input: "2", expected: "2" },
     nested_loop: { file: "nested_loop", input: "", expected: "-6" },
@@ -53,108 +56,66 @@ static_error_tests! {
     wrong_arg_count: { file: "wrong_arg_count", input: "", expected: "" },
 }
 
-
 repl_tests! {
-    // test_simple_bools: ["(define x true)", "x", "false"] => ["true", "false"],
+    test_simple_bools: {commands:["(define x true)", "x", "false"], expected: ["true", "false"]},
 
-    // // Basic function definition and call
-    // test_fun_simple: [
-    //     "(fun (double x) (+ x x))",
-    //     "(double 5)"
-    // ] => ["10"],
+    // Basic function definition and call
+    test_fun_simple: {commands:["(fun (double x) (+ x x))", "(double 5)"], expected: ["10"]},
 
-    // // Function with multiple arguments
-    // test_fun_multi_args: [
-    //     "(fun (add3 x y z) (+ (+ x y) z))",
-    //     "(add3 1 2 3)"
-    // ] => ["6"],
+    // Function with multiple arguments
+    test_fun_multi_args: {commands:["(fun (add3 x y z) (+ (+ x y) z))", "(add3 1 2 3)"], expected: ["6"]},
 
-    // // Recursive function
-    // test_fun_recursive: [
-    //     "(fun (fact n) (if (= n 0) 1 (* n (fact (sub1 n)))))",
-    //     "(fact 5)"
-    // ] => ["120"],
+    // Recursive function
+    test_fun_recursive: {commands:["(fun (fact n) (if (= n 0) 1 (* n (fact (sub1 n)))))", "(fact 5)"], expected: ["120"]},
 
-    // // Multiple functions calling each other
-    // test_fun_chain: [
-    //     "(fun (add2 x) (+ x 2))",
-    //     "(fun (mul3 x) (* x 3))",
-    //     "(mul3 (add2 5))"
-    // ] => ["21"],
+    // Multiple functions calling each other
+    test_fun_chain: {commands:["(fun (add2 x) (+ x 2))", "(fun (mul3 x) (* x 3))", "(mul3 (add2 5))"], expected: ["21"]},
 
-    // // Define variable and use it
-    // test_define_use: [
-    //     "(define x 42)",
-    //     "(+ x 8)"
-    // ] => ["50"],
+    // Define variable and use it
+    test_define_use: {commands:["(define x 42)", "(+ x 8)"], expected: ["50"]},
 
-    // // Multiple defines
-    // test_define_multiple: [
-    //     "(define a 10)",
-    //     "(define b 20)",
-    //     "(+ a b)"
-    // ] => ["30"],
+    // Multiple defines
+    test_define_multiple: {commands:["(define a 10)", "(define b 20)", "(+ a b)"], expected: ["30"]},
 
-    // // Set! on defined variable
-    // test_set_define: [
-    //     "(define counter 0)",
-    //     "counter",
-    //     "(set! counter 10)",
-    //     "counter"
-    // ] => ["0", "10"],
+    // Set! on defined variable
+    test_set_define: {commands:["(define counter 0)", "counter", "(set! counter 10)", "counter"], expected: ["0", "10"]},
 
-    // // Set! multiple times
-    // test_set_multiple: [
-    //     "(define x 1)",
-    //     "(set! x 2)",
-    //     "(set! x (+ x 3))",
-    //     "x"
-    // ] => ["5"],
+    // Set! multiple times
+    test_set_multiple: {commands:["(define x 1)", "(set! x 2)", "(set! x (+ x 3))", "x"], expected: ["5"]},
 
-    // // Function using defined variable
-    // test_fun_with_define: [
-    //     "(define base 100)",
-    //     "(fun (add_base x) (+ x base))",
-    //     "(add_base 23)"
-    // ] => ["123"],
+    // Function using defined variable
+    test_fun_with_define: {commands:["(define base 100)", "(fun (add_base x) (+ x base))", "(add_base 23)"], expected: ["123"]},
 
-    // // Function and set! on defined variable
-    // test_fun_set_define: [
-    //     "(define total 0)",
-    //     "(fun (add_to_total x) (set! total (+ total x)))",
-    //     "(add_to_total 5)",
-    //     "total",
-    //     "(add_to_total 10)",
-    //     "total"
-    // ] => ["5", "5", "15", "15"],
+    // Function and set! on defined variable
+    test_fun_set_define: {commands:["(define total 0)", "(fun (add_to_total x) (set! total (+ total x)))", "(add_to_total 5)", "total", "(add_to_total 10)", "total"], expected: ["5", "5", "15", "15"]},
 
-    // // Complex: function using and modifying defined variables
-    // test_fun_modify_globals: [
-    //     "(define x 10)",
-    //     "(define y 20)",
-    //     "(fun (swap) (let ((temp x)) (block (set! x y) (set! y temp) x)))",
-    //     "(swap)",
-    //     "x",
-    //     "y"
-    // ] => ["20", "20", "10"],
+    // Complex: function using and modifying defined variables
+    test_fun_modify_globals: {commands:["(define x 10)", "(define y 20)", "(fun (swap) (let ((temp x)) (block (set! x y) (set! y temp) x)))", "(swap)", "x", "y"], expected: ["20", "20", "10"]},
 
-    // // Print in REPL
-    // test_print_repl: [
-    //     "(fun (test_print x) (block (print x) (+ x 1)))",
-    //     "(test_print 5)"
-    // ] => ["5\n6"],
+    // Print in REPL
+    test_print_repl: {commands:["(fun (test_print x) (block (print x) (+ x 1)))", "(test_print 5)"], expected: ["5\n6"]},
 
-    // // Loop with defined variable
-    // test_loop_with_define: [
-    //     "(define result 1)",
-    //     "(loop (if (> result 10) (break result) (set! result (* result 2))))"
-    // ] => ["16"],
-
-    // // Function calling another function with defines
-    // test_nested_fun_define: [
-    //     "(define multiplier 3)",
-    //     "(fun (triple x) (* x multiplier))",
-    //     "(fun (triple_and_add y) (+ (triple y) 10))",
-    //     "(triple_and_add 4)"
-    // ] => ["22"],
+    test_define_and_use: { commands: ["(define a 10)", "(define b (+ a 5))", "(+ a b)"], expected: ["25"], typecheck: true },
+    repl_complicated_tc: { commands: [
+        "(define acc 0)",
+        "(fun (even (x : Num)) -> Num (set! acc (add1 acc)))",
+        "(fun (process (n : Num)) -> Num (let ((i 1)) (loop (if (> i n) (break acc) (block (even i) (set! i (+ i 1)))))))",
+        "(process 7)",
+        "(process 14)",
+        "(even 15)",
+        "(block (print 3) (+ 1 true))",
+        "acc"
+    ], expected: ["7", "21", "22", "22"], typecheck: true},
+repl_tc: {
+commands: [
+"(define x 7)",
+"x",
+"(set! x false)",
+"x",
+"(block (print 3) (+ 1 true))",
+"x"
+],
+expected: ["7", "7", "7"],
+typecheck: true
+},
 }
